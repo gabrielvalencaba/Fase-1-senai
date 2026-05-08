3b5eac0677fafe73eeab2de6fd46e01b3adffc71
gabrielvalencaba <gabrielvalenca209@gmail.com>
2026-05-07 22:06:47 -0300

    clt e dona bete

---
+++ 03-css/forcaTrabalho/script.js
@@ -0,0 +1,30 @@
+let resposta = document.getElementById('resultado')
+function verificarDevs(){
+
+// infos
+let clt, estagiarios, pj
+let totalDevs
+let mensagem = ''
+
+// leituras
+clt = Number(prompt("Quantos devs CLT?: "))
+estagiarios = Number(prompt("Quantos estagiários?: "))
+pj = Number(prompt("Quantos devs PJ?: "))
+
+totalDevs = clt + estagiarios + pj
+
+if(clt >= (totalDevs / 2)) {
+    // bateu a quantidade funcionarios
+    mensagem = 'Batemos a quantidade de funcionarios necessaria' + totalDevs
+}else{
+  // não bateu o minimo de funcionarios
+  mensagem = " Não bateu o numero de clt necessario"
+} 
+
+// saídas
+resposta.innerHTML = "Total de desenvolvedores: " + totalDevs + "<br>" + mensagem
+
+
+}
+
+
